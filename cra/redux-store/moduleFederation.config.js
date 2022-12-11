526b59cab0fc94daeb90fd3bf79cae9fa58b4a97
Surjit Kumar Sahoo <surjit@Surjits-MacBook-Pro.local>
2022-12-11 22:53:26 +0530

    created a react micro service to use as a centralized redux store

---
+++ cra/redux-store/moduleFederation.config.js
@@ -0,0 +1,23 @@
+const { dependencies } = require('./package.json');
+
+module.exports = {
+	name: 'central_redux_store', // change me
+	filename: 'remoteEntry.js',
+	exposes: {
+		'./store': './src/globalStore/index.ts',
+	},
+	remotes: {},
+	shared: {
+		...dependencies,
+		react: {
+			singleton: true,
+			import: 'react', // fallback is also react
+			shareScope: 'default',
+			requiredVersion: dependencies.react,
+		},
+		"react-dom": {
+			singleton: true,
+			requiredVersion: dependencies["react-dom"],
+		}
+	}
+}
