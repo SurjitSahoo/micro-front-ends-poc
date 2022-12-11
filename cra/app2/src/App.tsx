56ea721fc0acebf0cce2f2e846f10e5b3c644647
Surjit Kumar Sahoo <surjit@Surjits-MacBook-Pro.local>
2022-12-11 22:54:53 +0530

    wired up cra/app2 to use centralized redux store

--- cra/app2/src/App.tsx
+++ cra/app2/src/App.tsx
@@ -1,23 +1,30 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Hello from './mf/hello';
+import AppRoutes from 'mf/routes';
+
+// @ts-ignore
+import { CentralStoreProvider } from 'redux/store';
 
 function App() {
   return (
-    <div className='bg-gray-600 pt-10 text-white min-h-screen'>
-      <div className='max-w-6xl text-3xl mx-auto'>
-        <div>Name: App2 CRA</div>
-        <div>Framework: react</div>
-        <div>Language: TypeScript</div>
-        <div className='mb-10'>CSS: Tailwind</div>
+    <CentralStoreProvider>
+      <div className='bg-gray-600 pt-10 text-white min-h-screen'>
+        <div className='max-w-6xl text-3xl mx-auto'>
+          <div>Name: App2 CRA</div>
+          <div>Framework: react</div>
+          <div>Language: TypeScript</div>
+          <div className='mb-10'>CSS: Tailwind</div>
 
-        <BrowserRouter>
-          <Hello />
-          <Routes>
-            <Route path='/test' element={<div>test route</div>} />
-          </Routes>
-        </BrowserRouter>
+          <BrowserRouter>
+            <Hello />
+            <Routes>
+              <Route path='/test' element={<div>test route</div>} />
+            </Routes>
+            <AppRoutes />
+          </BrowserRouter>
+        </div>
       </div>
-    </div>
+    </CentralStoreProvider>
   );
 }
 
